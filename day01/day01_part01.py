0dba8593f6087e812d96b6a0d378440cbc0cbe39
Rich B <rich@royalnavy.io>
2023-12-04 21:47:41 +0000

    passable, working solution

--- day01/day01_part01.py
+++ day01/day01_part01.py
@@ -5,32 +5,92 @@ data = []
 valid_pswds = []
 invalid_pswds = []
 
+def get_numerical_digit(digit: str) -> int:
+    """ Retrieves the numerical digit """
+
+    lookup = {"one":1,
+              "two":2,
+              "three":3,
+              "four":4,
+              "five":5,
+              "six":6,
+              "seven":7,
+              "eight":8,
+              "nine":9}
+
+    return lookup[digit]
+
 def get_first_digit(cal_val: str) -> int:
-    """ Gets first digit """
-    digits = re.findall(r'^\D*(\d)', cal_val)
-    if len(digits) == 0:
-        raise IndexError
-    else:
-        first_digit = int(digits[0])
-    return first_digit
+    """ Gets first digit where the digit can be numerical or text """
+
+    # Set the min index to be the length of the input
+    min_index = len(cal_val)
+    match_value = None
+
+    # Find the lowest index position of a number word
+    for num in ['one','two','three','four','five','six','seven','eight','nine']:
+        matched_text = re.finditer(rf'({num})', cal_val)
+        for candidate in matched_text:
+            match_idx = candidate.start()
+            if match_idx < min_index:
+                min_index = match_idx
+                match_value = candidate.group()
+    
+    if match_value:
+        match_value = get_numerical_digit(match_value)
+
+    # Find the lowest index position of a number digit
+    pattern = r'(\d)'
+    matched_numbers = re.finditer(pattern, cal_val)
+    for candidate in matched_numbers:
+        match_idx = candidate.start()
+        if match_idx < min_index:
+            min_index = match_idx
+            match_value = candidate.group()
+
+    return int(match_value)
 
 def get_last_digit(cal_val: str) -> int:
-    """ Gets last digit """
-    digits = re.findall(r'(\d)', cal_val)
-    if len(digits) == 0:
-        raise IndexError
-    else:
-        last_digit = int(digits[-1])
-    return last_digit
+    """ Gets last digit where the digit can be numerical or text """
 
-total_cal_value = 0
+    # Set the min index to be the length of the input
+    min_index = -1
+    match_value = None
 
-for line in f.readlines():
-    line = line.strip()
-    first = get_first_digit(line)
-    last = get_last_digit(line)
-    cal_val = int(str(first) + str(last))
-    print(line, first, last, cal_val)
-    total_cal_value += cal_val
+    # Find the lowest index position of a number word
+    for num in ['one','two','three','four','five','six','seven','eight','nine']:
+        matched_text = re.finditer(rf'({num})', cal_val)
+        for candidate in matched_text:
+            match_idx = candidate.start()
+            if match_idx > min_index:
+                min_index = match_idx
+                match_value = candidate.group()
 
-print (total_cal_value)
+    if match_value:
+        match_value = get_numerical_digit(match_value)
+
+    # Find the lowest index position of a number digit
+    pattern = r'(\d)'
+    matched_numbers = re.finditer(pattern, cal_val)
+    for candidate in matched_numbers:
+        match_idx = candidate.start()
+        if match_idx > min_index:
+            min_index = match_idx
+            match_value = candidate.group()
+
+    return int(match_value)
+
+
+if __name__ == '__main__':
+
+    total_cal_value = 0
+
+    for line in f.readlines():
+        line = line.strip()
+        first = get_first_digit(line)
+        last = get_last_digit(line)
+        cal_val = int(str(first) + str(last))
+        print(line, first, last, cal_val)
+        total_cal_value += cal_val
+
+    print (total_cal_value)
--- day01/test_day01_part01.py
+++ day01/test_day01_part01.py
@@ -3,49 +3,45 @@ import day01_part01
 
 class TestDay01Part01(unittest.TestCase):
     """ """
-    def test_get_first_digit(self):
+    def get_first_digit(self):
         """ Tests the get_first_digit function """
 
         test_data = [
             {"cal_val": "1a2", "truth": 1},
             {"cal_val": "1a", "truth": 1},
             {"cal_val": "a1a", "truth": 1},
-            {"cal_val": "12a", "truth": 1}
+            {"cal_val": "12a", "truth": 1},
+            {"cal_val": "onetwo2", "truth": 1},
+            {"cal_val": "1two", "truth": 1},
+            {"cal_val": "aa1two", "truth": 1},
+            {"cal_val": "three1two", "truth": 3},
+            {"cal_val": "oneeight2", "truth": 1}
             ]
-        
+
         for data in test_data:
+            print (data["cal_val"])
             res = day01_part01.get_first_digit(data["cal_val"])
             self.assertEqual(res, data["truth"])
 
     def test_get_last_digit(self):
-        """Tests the get_last_digit function """
+        """ Tests the get_last_digit function """
 
         test_data = [
             {"cal_val": "1a2", "truth": 2},
             {"cal_val": "1a", "truth": 1},
             {"cal_val": "a1a", "truth": 1},
             {"cal_val": "12a", "truth": 2},
-            {"cal_val": "12a1a3", "truth": 3}
+            {"cal_val": "onetwo2", "truth": 2},
+            {"cal_val": "1two", "truth": 2},
+            {"cal_val": "aa1two", "truth": 2},
+            {"cal_val": "three1two", "truth": 2},
+            {"cal_val": "2oneight", "truth": 8}
             ]
-        
+
         for data in test_data:
+            print (data["cal_val"])
             res = day01_part01.get_last_digit(data["cal_val"])
             self.assertEqual(res, data["truth"])
-
-    def test_get_first_digit_error(self):
-        """Tests the get_first_digit function with a faulty line"""
-
-        test_data = [{"cal_val": "a"}]
-        for data in test_data:
-            #day01_part01.get_first_digit(data["cal_val"])
-            self.assertRaises(IndexError, day01_part01.get_first_digit, data["cal_val"])
-
-    def test_get_last_digit_error(self):
-        """Tests the get_last_digit function with a faulty line"""
-
-        test_data = [{"cal_val": "a"}]
-        for data in test_data:
-            self.assertRaises(IndexError, day01_part01.get_last_digit, data["cal_val"])
 
 if __name__ == '__main__':
     unittest.main()
